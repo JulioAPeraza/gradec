a0cdf4155977cc73f8c2091ff4e8d9649e69477d
JulioAPeraza <jpera054@fiu.edu>
2023-06-26 19:22:46 -0400

    test decoder

--- gradec/__init__.py
+++ gradec/__init__.py
@@ -1,6 +1,8 @@
 """Gradec: Meta-analytic gradient decoding."""
-from . import segmentation
+from . import decode, fetcher, segmentation
 
 __all__ = [
     "segmentation",
+    "decode",
+    "fetcher",
 ]
