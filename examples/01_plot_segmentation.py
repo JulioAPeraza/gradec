0bee00b228fa7f93d4e6dfdf465c8a90a4b996d9
JulioAPeraza <jpera054@fiu.edu>
2024-02-24 18:59:42 -0500

    Add support for multidimensional segmentation

--- examples/01_plot_segmentation.py
+++ examples/01_plot_segmentation.py
@@ -10,21 +10,20 @@ Segmentation of the Cortical Gradient
 Gradec provides two methods for splitting the gradient spectrum.
 """
 
-from gradec.fetcher import _fetch_principal_gradients
-
-###############################################################################
 # Download HCP S1200 principal gradient
-# -----------------------------------------------------------------------------
+from gradec.fetcher import _fetch_gradients
 
-principal_gradient = _fetch_principal_gradients()
-
+gradients = _fetch_gradients(cortical=True)
 
 ###############################################################################
-# Plot principal gradient
-# ``````````````````````````````````````````````````````````````````````````````
+# 1D Segmentation
+# -----------------------------------------------------------------------------
+# Select and plot the principal gradient
 from surfplot.utils import add_fslr_medial_wall
 
 from gradec.plot import plot_surf_maps
+
+principal_gradient = gradients[:, 0]
 
 full_vertices = 64984
 hemi_vertices = full_vertices // 2
@@ -36,19 +35,80 @@ fig = plot_surf_maps(prin_grad_lh, prin_grad_rh)
 fig.show()
 
 ###############################################################################
-# Segment the principal gradient
+# KMeans-based segmentation
 # ``````````````````````````````````````````````````````````````````````````````
 from gradec.segmentation import KMeansSegmentation
 
-segmentation = KMeansSegmentation(2)
-segmentation.fit(principal_gradient)
-grad_maps = segmentation.transform()
+k_segmentation = KMeansSegmentation(n_segments=2)
+k_segmentation.fit(principal_gradient)
+k_grad_maps = k_segmentation.transform()
+
+for map_i, k_grad_map in enumerate(k_grad_maps):
+    k_grad_map = add_fslr_medial_wall(k_grad_map)
+    k_grad_map_lh, k_grad_map_rh = (
+        k_grad_map[:hemi_vertices],
+        k_grad_map[hemi_vertices:full_vertices],
+    )
+    fig = plot_surf_maps(
+        k_grad_map_lh,
+        k_grad_map_rh,
+        color_range=(0, 1),
+        cmap="YlOrRd",
+        title=f"KMeans, segments={map_i}",
+    )
+    fig.show()
 
 ###############################################################################
-# Plot gradients maps
+# PCT-based segmentation
 # ``````````````````````````````````````````````````````````````````````````````
-for grad_map in grad_maps:
-    grad_map = add_fslr_medial_wall(grad_map)
-    grad_map_lh, grad_map_rh = grad_map[:hemi_vertices], grad_map[hemi_vertices:full_vertices]
-    fig = plot_surf_maps(grad_map_lh, grad_map_rh)
+from gradec.segmentation import PCTSegmentation
+
+p_segmentation = PCTSegmentation(n_segments=2)
+p_segmentation.fit(principal_gradient)
+p_grad_maps = p_segmentation.transform()
+
+for map_i, p_grad_map in enumerate(p_grad_maps):
+    p_grad_map = add_fslr_medial_wall(p_grad_map)
+    p_grad_map_lh, p_grad_map_rh = (
+        p_grad_map[:hemi_vertices],
+        p_grad_map[hemi_vertices:full_vertices],
+    )
+    fig = plot_surf_maps(
+        p_grad_map_lh,
+        p_grad_map_rh,
+        color_range=(0, 1),
+        cmap="YlOrRd",
+        title=f"PCT, segments={map_i}",
+    )
+    fig.show()
+
+
+###############################################################################
+# Multidimensional Segmentation
+# -----------------------------------------------------------------------------
+# Select the first four gradients
+gradients_4d = gradients[:, :4]
+
+###############################################################################
+# KMeans-based segmentation
+# ``````````````````````````````````````````````````````````````````````````````
+from gradec.segmentation import KMeansSegmentation
+
+k4_segmentation = KMeansSegmentation(n_segments=4)
+k4_segmentation.fit(gradients_4d)
+k4_grad_maps = k4_segmentation.transform()
+
+for map_i, k4_grad_map in enumerate(k4_grad_maps):
+    k4_grad_map = add_fslr_medial_wall(k4_grad_map)
+    k4_grad_map_lh, k3_grad_map_rh = (
+        k4_grad_map[:hemi_vertices],
+        k4_grad_map[hemi_vertices:full_vertices],
+    )
+    fig = plot_surf_maps(
+        k4_grad_map_lh,
+        k3_grad_map_rh,
+        color_range=(0, 1),
+        cmap="YlOrRd",
+        title=f"KMeans, cluster={map_i}",
+    )
     fig.show()
--- gradec/tests/test_segmentation.py
+++ gradec/tests/test_segmentation.py
@@ -9,9 +9,10 @@ from gradec.segmentation import KMeansSegmentation, PCTSegmentation
 @pytest.mark.parametrize(
     "segmentation",
     [
-        (PCTSegmentation(10)),
-        (KMeansSegmentation(10)),
+        (PCTSegmentation(n_segments=10)),
+        (KMeansSegmentation(n_segments=10)),
     ],
+    ids=["PCT", "KMeans"],
 )
 def test_segmentation_smoke(segmentation):
     """Smoke test for segmentation."""
@@ -21,3 +22,22 @@ def test_segmentation_smoke(segmentation):
 
     assert len(grad_maps) == 10
     assert all(len(grad_map) == 100 for grad_map in grad_maps)
+
+
+def test_segmentation_kmeans_highdim():
+    """Test segmentation with high-dimensional gradient."""
+    gradients = np.random.rand(100, 4)
+
+    segmentation = KMeansSegmentation(n_segments=10).fit(gradients)
+    grad_maps = segmentation.transform()
+
+    assert len(grad_maps) == 10
+    assert all(len(grad_map) == 100 for grad_map in grad_maps)
+
+
+def test_segmentation_pct_highdim():
+    """Test segmentation with high-dimensional gradient."""
+    gradients = np.random.rand(100, 10)
+
+    with pytest.raises(ValueError):
+        PCTSegmentation(n_segments=10).fit(gradients)
